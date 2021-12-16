2748a82416a2a061d2fec1aef4c5b3d508e7a111
rmhyde@gmail.com <robert.hyde@citrix.com>
2021-12-16 00:43:02 +0000

    Day 8 Init

---
+++ Day8/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day8
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

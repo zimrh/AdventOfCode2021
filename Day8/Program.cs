d47bc8fae08f9772e1971fe1bf398b3f7c61cda0
rmhyde@gmail.com <robert.hyde@citrix.com>
2021-12-16 01:04:30 +0000

    Day 8 Part 1

--- Day8/Program.cs
+++ Day8/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
 namespace Day8
 {
@@ -6,7 +8,35 @@ namespace Day8
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var displayDataFilePath = "segments.txt";
+            var easyNumbersCount = Get1478Count(displayDataFilePath);
+            Console.WriteLine($"Easy Number Count: {easyNumbersCount}");
+        }
+
+        private static int Get1478Count(string displayDataFilePath)
+        {
+            var numbersCount = 0;
+
+            using var displayDataFile = File.OpenText(displayDataFilePath);
+            do
+            {
+                var line = displayDataFile.ReadLine();
+                var displays = line.Split("|");
+                var segments = displays[1].Split(" ");
+                foreach (var segment in segments)
+                {
+                    var len = segment.Length;
+                    if (len == 2 // 1 Segment
+                        || len == 4 // 4 Segment
+                        || len == 3 // 7 Segment
+                        || len == 7) // 8 Segment
+                    {
+                        numbersCount++;
+                    }
+                }
+            } while (!displayDataFile.EndOfStream);
+
+            return numbersCount;
         }
     }
 }

1279b26846ee3fb1f0d49844e9515b0e1714a12f
rmhyde@gmail.com <robert.hyde@citrix.com>
2022-01-10 14:37:47 +0000

    Day 12 Part 2 (Bit rough and ready but it works)

--- Day12/Program.cs
+++ Day12/Program.cs
@@ -15,6 +15,19 @@ namespace Day12
             const string largeCaveTestFilePath = "cave-map-large.txt";
             var caveTestFilePath = "actual-cave-map.txt";
 
+            var partTwoSmallCavePathCount = GetPathCountsWithOneRevisit(smallCaveTestFilePath);
+            Console.WriteLine($"Part Two: Small Cave Path Count: {partTwoSmallCavePathCount}");
+            
+            var partTwoMediumCavePathCount = GetPathCountsWithOneRevisit(mediumCaveTestFilePath);
+            Console.WriteLine($"Part Two: Medium Cave Path Count: {partTwoMediumCavePathCount}");
+
+            var partTwoLargeCavePathCount = GetPathCountsWithOneRevisit(largeCaveTestFilePath);
+            Console.WriteLine($"Part Two: Large Cave Path Count: {partTwoLargeCavePathCount}");
+            
+            var partTwoActualCavePathCount = GetPathCountsWithOneRevisit(caveTestFilePath);
+            Console.WriteLine($"Part Two: Actual Cave Path Count: {partTwoActualCavePathCount}");
+
+
             var partOneSmallCavePathCount = GetPathCounts(smallCaveTestFilePath);
             Console.WriteLine($"Part One: Small Cave Path Count: {partOneSmallCavePathCount}");
             
@@ -22,21 +35,40 @@ namespace Day12
             Console.WriteLine($"Part One: Medium Cave Path Count: {partOneMediumCavePathCount}");
             
             var partOneLargeCavePathCount = GetPathCounts(largeCaveTestFilePath);
-            Console.WriteLine($"Part One: Small Cave Path Count: {partOneLargeCavePathCount}");
+            Console.WriteLine($"Part One: Large Cave Path Count: {partOneLargeCavePathCount}");
 
             var partOneActualCavePathCount = GetPathCounts(caveTestFilePath);
-            Console.WriteLine($"Part One: Small Cave Path Count: {partOneActualCavePathCount}");
+            Console.WriteLine($"Part One: Actual Cave Path Count: {partOneActualCavePathCount}");
 
+        }
+
+        private static int GetPathCountsWithOneRevisit(string caveMapFilePath)
+        {
+            var paths = new List<string>();
+            var caves = PopulateCaves(caveMapFilePath);
+            foreach (var smallCave in caves.Where(c => !caves[c.Key].IsBigCave).Select(c => c.Key))
+            {
+                var potentialPaths = GetPaths(caves, smallCave);
+                foreach (var potentialPath in potentialPaths)
+                {
+                    var test = string.Join(",", potentialPath);
+                    if (!paths.Contains(test))
+                    {
+                        paths.Add(test);
+                    }
+                }
+            }
+            return paths.Count;
         }
 
         private static int GetPathCounts(string caveMapFilePath)
         {
             var caves = PopulateCaves(caveMapFilePath);
-            var paths = GetPaths(caves);
+            var paths = GetPaths(caves, string.Empty);
             return paths.Count;
         }
 
-        private static List<List<string>> GetPaths(Dictionary<string, Cave> caves)
+        private static List<List<string>> GetPaths(Dictionary<string, Cave> caves, string caveToVisitTwice)
         {
             var currentPaths = new List<List<string>>
             {
@@ -62,7 +94,7 @@ namespace Day12
 
                     foreach (var connectedCave in caves[currentPath.Last()].ConnectedCaves)
                     {
-                        if (!CanWeVisitThisCave(caves, currentPath, connectedCave))
+                        if (!CanWeVisitThisCave(caves, currentPath, connectedCave, caveToVisitTwice))
                         {
                             continue;
                         }
@@ -83,7 +115,7 @@ namespace Day12
             return currentPaths;
         }
 
-        private static bool CanWeVisitThisCave(IReadOnlyDictionary<string, Cave> caves, List<string> currentPath, string connectedCave)
+        private static bool CanWeVisitThisCave(IReadOnlyDictionary<string, Cave> caves, List<string> currentPath, string connectedCave, string caveToVisitTwice)
         {
             if (string.Equals(connectedCave, Cave.StartCaveId))
             {
@@ -106,7 +138,12 @@ namespace Day12
 
                 caveVisitCount[step]++;
 
-                if (caveVisitCount[step] > 1)
+                if (caveVisitCount[step] > 1 && !step.Equals(caveToVisitTwice))
+                {
+                    return false;
+                }
+
+                if (step.Equals(caveToVisitTwice) && caveVisitCount[step] > 2)
                 {
                     return false;
                 }

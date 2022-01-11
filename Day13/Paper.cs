45f0a4526a34f4dfc5c882074a365a959f9d972f
rmhyde@gmail.com <robert.hyde@citrix.com>
2022-01-11 12:11:16 +0000

    Day 13: Parts 1 and 2

---
+++ Day13/Paper.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day13
+{
+    internal class Paper
+    {
+        public bool[,] Sheet { get; set; }
+        
+        public List<string> FoldInstructions { get; set; } = new List<string>();
+
+        public int CurrentWidth { get; set; }
+        
+        public int CurrentHeight { get; set; }
+
+        public void FoldLeftAtX(int col)
+        {
+            for (var y = 0; y < CurrentHeight; y++)
+            {
+                for (var x = 1; x < CurrentWidth - col; x++)
+                {
+                    Sheet[col - x, y] = Sheet[col - x, y] || Sheet[col + x, y];
+                }
+            }
+
+            CurrentWidth = col;
+        }
+        
+        public void FoldUpAtY(int row)
+        {
+            for (var x = 0; x < CurrentWidth; x++)
+            {
+                for (var y = 1; y < CurrentHeight - row; y++)
+                {
+                    Sheet[x, row - y] = Sheet[x, row - y] || Sheet[x, row + y];
+                }
+            }
+
+            CurrentHeight = row;
+        }
+        
+        public int GetHoleCount()
+        {
+            var count = 0;
+            for (var y = 0; y < CurrentHeight; y++)
+            {
+                for (var x = 0; x < CurrentWidth; x++)
+                {
+                    count = count + (Sheet[x, y] ? 1 : 0);
+                }
+            }
+
+            return count;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            for (var y = 0; y < CurrentHeight; y++)
+            {
+                for (var x = 0; x < CurrentWidth; x++)
+                {
+                    sb.Append(Sheet[x, y] ? "#": ".");
+                }
+                sb.AppendLine();
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Day13/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Day13
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var testInputFilePath = "test-input.txt";
+            var testPaperAfterAllFolds = GetPaperAfterFolding(testInputFilePath);
+            Console.WriteLine($"Test paper after all folds: {Environment.NewLine}{testPaperAfterAllFolds}");
+
+            Console.WriteLine();
+            
+            var actualInputFilePath = "actual-input.txt";
+            var sheetAfterFirstFold = GetPaperAfterFolding(actualInputFilePath, true);
+            var holeCountAfterFirstFold = sheetAfterFirstFold.Count(c => c.Equals('#'));
+            Console.WriteLine($"Part 1: Hole count after first fold: {holeCountAfterFirstFold}");
+
+            Console.WriteLine();
+            
+            var sheetAfterAllFolds = GetPaperAfterFolding(actualInputFilePath);
+            Console.WriteLine($"Sheet after all folds: {Environment.NewLine}{sheetAfterAllFolds}");
+
+        }
+
+        private static string GetPaperAfterFolding(string inputFilePath, bool firstFoldCount = false)
+        {
+            var paper = GetPaper(inputFilePath);
+
+            foreach (var foldInstruction in paper.FoldInstructions)
+            {
+                var axis = foldInstruction.Split("=").First();
+                var position = int.Parse(foldInstruction.Split("=").Last());
+
+                if (axis.Equals("x"))
+                {
+                    paper.FoldLeftAtX(position);
+                }
+                else
+                {
+                    paper.FoldUpAtY(position);
+                }
+
+                if (firstFoldCount)
+                {
+                    return paper.ToString();
+                }
+            }
+            
+            return paper.ToString();
+        }
+
+        private static Paper GetPaper(string inputFilePath)
+        {
+            using var inputFile = File.OpenText(inputFilePath);
+            var inputPage = new bool[1500, 1500];
+            var paper = new Paper();
+            var pageWidth = 0;
+            var pageHeight = 0;
+
+            do
+            {
+                var line = inputFile.ReadLine();
+                if (line.StartsWith("fold along "))
+                {
+                    paper.FoldInstructions.Add(line.Substring(11, line.Length - 11));
+                }
+                else
+                {
+                    var split = line.Split(',', StringSplitOptions.RemoveEmptyEntries);
+                    
+                    if (split.Length != 2) continue;
+
+                    var x = int.Parse(split.First());
+                    pageWidth = (x > pageWidth) ? x : pageWidth;
+                    var y = int.Parse(split.Last());
+                    pageHeight = (y > pageHeight) ? y : pageHeight;
+                    inputPage[x, y] = true;
+                }
+
+            } while (!inputFile.EndOfStream);
+
+            inputFile.Close();
+
+            paper.CurrentHeight = pageHeight + 1;
+            paper.CurrentWidth = pageWidth + 1;
+
+            paper.Sheet = new bool[paper.CurrentWidth, paper.CurrentHeight];
+
+            for (var x = 0; x < paper.CurrentWidth; x++)
+            {
+                for (var y = 0; y < paper.CurrentHeight; y++)
+                {
+                    paper.Sheet[x, y] = inputPage[x, y];
+                }
+            }
+
+            return paper;
+        }
+    }
+
+}
